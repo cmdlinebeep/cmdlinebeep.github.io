5fd8a6b30adc6cc27ffd26651f1a6ae9ea475f85
cmdlinebeep <joelwigton@gmail.com>
2020-02-04 16:56:32 -0700

    added ux version to ks

---
+++ ks/assets/js/typewriter-custom.js
@@ -0,0 +1,10 @@
+/* ========= Typewriter effect =========== */
+/* Ref: https://safi.me.uk/typewriterjs/ */
+const instance = new Typewriter('#typewriter', {
+  // strings: ['JS Library', 'jQuery Plugin', 'Browser Extension', 'CMS Plugin', 'Mobile App', 'Open Source Project'],
+  strings: ['The Coloradoan', 'The Gazette', 'The Denver Post', 'The Wall Street Journal'],
+  autoStart: true,
+  loop: true,
+});
+
+
